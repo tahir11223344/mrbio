c57651c03708b45e7b3375072ce3c4e3e2edf057
usmanmetait-code <usmanmetait@gmail.com>
2026-02-23 20:14:38 +0500

    google tag manager added to mrbiomed tech services

--- resources/views/frontend/layouts/frontend.blade.php
+++ resources/views/frontend/layouts/frontend.blade.php
@@ -20,6 +20,14 @@
 
     <meta name="author" content="" />
 
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-Z1HSV26QCJ"></script>
+    <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-Z1HSV26QCJ');
+    </script>
+
     <!-- Favicons -->
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('storage/' . setting('favicon')) }}">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('storage/' . setting('favicon')) }}">
