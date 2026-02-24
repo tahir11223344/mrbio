1537b40f5592a7dcbd0dbc065d6b1c6511235a01
usmanmetait-code <usmanmetait@gmail.com>
2026-02-24 20:43:01 +0500

    more google tags added to the head of the website

--- resources/views/frontend/layouts/frontend.blade.php
+++ resources/views/frontend/layouts/frontend.blade.php
@@ -6,12 +6,20 @@
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-Z1HSV26QCJ"></script>
     <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
 
-    gtag('config', 'G-Z1HSV26QCJ');
+        gtag('config', 'G-Z1HSV26QCJ');
     </script>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-M68N96GZ');</script>
+    <!-- End Google Tag Manager -->
+
 
     <base href="" />
     <title>@yield('meta_title', config('app.name'))</title>
@@ -82,6 +90,10 @@
 
 <body {!! printHtmlClasses('body') !!} {!! printHtmlAttributes('body') !!}>
 
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M68N96GZ"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     @include('partials/theme-mode/_init')
 
     @include('frontend.layouts.partials.header')
